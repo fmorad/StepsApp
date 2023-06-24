787fc3be555c2c22532cc528db53dccf67911a5d
fmorad <fmorad@hct.ac.ae>
2023-06-24 14:05:19 +0400

    Initial Commit

---
+++ StepsApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StepsApp
+//
+//  Created by Fatmah Morad on 24/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StepsApp/StepsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepsAppApp.swift
+//  StepsApp
+//
+//  Created by Fatmah Morad on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct StepsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
